2293eaa8c570381285d9321240d8d7dcf81556da
ReduxFlakes <tiagompca@proton.me>
2023-02-23 23:01:13 +0000

    new version

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const markdownIt = require("markdown-it");
 const markdownItAnc = require("markdown-it-anchor");
 const { DateTime } = require("luxon");
-const striptags = require("striptags");
 
 
 module.exports = function (eleventyConfig) {
@@ -40,23 +39,3 @@ module.exports = function (eleventyConfig) {
     dataTemplateEngine: "njk",
   };
 };
-
-function extractExcerpt(article) {
-  if (!article.hasOwnProperty("templateContent")) {
-    console.warn(
-      'Failed to extract excerpt: Document has no property "templateContent".'
-    );
-    return null;
-  }
-
-  let excerpt = null;
-  const content = article.templateContent;
-
-  excerpt = striptags(content)
-    .substring(0, 200) // Cap at 200 characters
-    .replace(/^\s+|\s+$|\s+(?=\s)/g, "")
-    .trim()
-    .concat("...");
-  return excerpt;
-}
-
--- src/about.md
+++ src/about.md
@@ -1,4 +1,6 @@
 ---
-layout: page.njk
+layout: minimal.njk
 title: About
 ---
+
+Hey, I'm ReduxFlakes and this is my cozy little corner of the internet. I'm a 16-year-old computer science student from Portugal, and I'm really into all things web, Android, and Python development. In my spare time, I like to contribute to the open source community and I'm a FOSS as well as privacy advocate, so fair warning, you might see me get a little fired up when it comes to proprietary software and privacy issues. Btw I **LOVE** music!
